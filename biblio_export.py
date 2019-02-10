a6c41f659d9566694b77897c2c9732a14a55916f
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2019-02-10 15:06:08 -0700

    wrote python script to produce html page from all bib files

---
+++ biblio_export.py
@@ -0,0 +1,112 @@
+# MIT License
+
+# Copyright (c) 2019 Benjamin Bercovici and Jay McMahon
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import bibtexparser
+from bibtex_entries_defs import *
+import glob, os
+
+
+# Creating empty containers
+journal_articles = []
+conference_proceedings = []
+misc = []
+mislabeled = []
+theses = []
+
+for file in glob.glob("*.bib"):
+    
+    # Reading from the bib file storing all of ORCCA's publications to date
+    with open(file) as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
+
+    # Put each publication in the correct category
+    for entry in bib_database.entries:
+        if entry["ENTRYTYPE"] == "inproceedings":
+            conference_proceedings += [entry]
+        elif entry["ENTRYTYPE"] == "article":
+            journal_articles += [entry]
+        elif entry["ENTRYTYPE"] == "misc":
+            misc += [entry]
+        elif entry["ENTRYTYPE"] == "phdthesis":
+            theses += [entry]
+        else:
+            mislabeled += [entry]
+
+# Sorting each publication type by year
+conference_proceedings = sorted(conference_proceedings, key = sort_by_year)[::-1] 
+journal_articles = sorted(journal_articles, key = sort_by_year)[::-1] 
+misc = sorted(misc, key = sort_by_year)[::-1] 
+theses = sorted(theses, key = sort_by_year) [::-1]
+
+
+# Lists any entry whose "ENTRYTYPE" is not amongst ["inproceedings" ,"article", "misc", "phdthesis"]
+if (len(mislabeled) > 0):
+    print("Mislabeled entries: ")
+    for entry in mislabeled:
+        print(entry)
+
+# Start Writing the HTML page
+page = []
+
+# Journal papers
+page += ["<p class=\"lead\">Journal Papers</p>\n"]
+page += ["<ol>\n"]
+for journal_entry in journal_articles:
+    page += create_journal_entry(journal_entry)
+page += ["</ol>\n"]
+
+# Conference proceedings
+page += ["<p class=\"lead\">Conference Proceedings</p>\n"]
+page += ["<ol>\n"]
+for conference_proceedings_entry in conference_proceedings:
+    page += create_proceedings_entry(conference_proceedings_entry)
+page += ["</ol>\n"]
+
+# Theses
+page += ["<p class=\"lead\">Theses</p>\n"]
+page += ["<ol>\n"]
+for theses_entry in theses:
+    page += create_thesis_entry(theses_entry)
+page += ["</ol>\n"]
+
+# Misc
+page += ["<p class=\"lead\">Miscellaneous</p>\n"]
+page += ["<ol>\n"]
+for misc_entry in misc:
+    page += create_misc_entry(misc_entry)
+page += ["</ol>\n"]
+
+# Show some statistics
+print("ORCCA Publications count:\n")
+print ("\t" + str(len(conference_proceedings)) + " conference proceedings")
+print ("\t" + str(len(journal_articles)) + " journal articles")
+print ("\t" + str(len(misc)) + " miscellaneous entries")
+print ("\t" + str(len(mislabeled)) + " mislabeled entries")
+
+# Saving the page
+with open('orcca_publications.html', 'w') as f:
+    for item in page:
+        f.write("%s" % item)
+
+
+
---
+++ bibtex_entries_defs.py
@@ -0,0 +1,209 @@
+# MIT License
+
+# Copyright (c) 2019 Benjamin Bercovici and Jay McMahon
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# <li>Bercovici, B., & Mcmahon, J. W. (2019). Inertia Parameter Statistics of An Uncertain Small Body Shape (Under Review). <i>Icarus</i>.<br />
+#         [expand title="BibTex" style="small"] [box color="lightgray" style="filled" float="none"]<br />
+#         @article{Bercovicib,<br />
+#         author = {Bercovici, Benjamin and Mcmahon, Jay W.},<br />
+#         file = {:Users/bbercovici/GDrive/CUBoulder/Research/papers/shape{\_}uncertainty/R0/main{\_}document.pdf:pdf},<br />
+#         journal = {Icarus},<br />
+#         keywords = {bezier triangles,inertia,shape model,small body,uncertainty},<br />
+#         title = {{Inertia Parameter Statistics of An Uncertain Small Body Shape (Under Review)}},<br />
+#         year = {2019}<br />
+#         } [/box]<font face="monospace"> </font>[/expand]</li>
+
+from bibtexparser.bibdatabase import as_text
+import bibtexparser
+
+def sort_by_year(entry):
+	try:
+		return int(entry["year"])
+	except KeyError:
+		try:
+
+			return int(entry["Year"])
+		except KeyError:
+			return 1970
+
+def latex_entry_to_html_box(entry):
+    string = "<br />[expand title=\"BibTex\" style=\"small\"] [box color=\"lightgray\" style=\"filled\" float=\"none\"]<br />"
+
+    entry_with_breaks = entry
+
+    for key in entry_with_breaks:
+        entry_with_breaks[key] += "<br />"
+    
+    string += as_text(entry)
+
+
+    string += "[/box]<font face=\"monospace\"> </font>[/expand]</li><br />"
+    return string
+
+
+
+def create_journal_entry(entry):
+    
+    # Starting item
+    formatted_entry = "\n<li>"
+
+    # Authors
+    formatted_entry += entry["author"].replace(" and ",", ")
+
+    # Year
+    try:
+        formatted_entry += " (" + str(entry['year']) + "). "
+    except KeyError:
+        try:
+            formatted_entry += " (" + str(entry['Year']) + "). "
+        except KeyError:
+            print("No year found in " + str(entry["ID"]))
+
+    # Title
+    formatted_entry +=  entry["title"].replace("{","").replace("}","")
+
+    # Journal 
+    formatted_entry += ". <i>" + entry["journal"] +  "</i>"
+
+    # DOI
+    try:
+        formatted_entry += " " + entry["doi"]
+    except KeyError:
+        try:
+            formatted_entry += " " + entry["DOI"]
+        except KeyError:
+            print("No doi found in " + str(entry["ID"]))
+
+
+    # Bibtex box
+    formatted_entry += latex_entry_to_html_box(entry)
+
+    # Closing item
+    formatted_entry += "</li>\n"
+
+
+    return formatted_entry
+
+def create_proceedings_entry(entry):
+
+    # Starting item
+    formatted_entry = "\n<li>"
+
+    # Authors
+    formatted_entry += entry["author"].replace(" and ",", ")
+
+    # Year
+    try:
+        formatted_entry += " (" + str(entry['year']) + "). "
+    except KeyError:
+        try:
+            formatted_entry += " (" + str(entry['Year']) + "). "
+        except KeyError:
+            print("No year found in " + str(entry["ID"]))
+
+    # Title
+    formatted_entry += " " + entry["title"].replace("{","").replace("}","")
+
+    # Proceedings 
+    formatted_entry += ". <i>" + entry["booktitle"] +  "</i>. "
+   
+    # Address of conferences
+    try:
+        formatted_entry += entry["address"]
+    except KeyError:
+        print ("No address found in " + str(entry["ID"]))
+
+    # Bibtex box
+    formatted_entry += latex_entry_to_html_box(entry)
+
+    # Closing item
+    formatted_entry += "</li>\n"
+
+
+    return formatted_entry
+
+def create_thesis_entry(entry):
+
+    # Starting item
+    formatted_entry = "\n<li>"
+
+    # Authors
+    formatted_entry += entry["author"].replace(" and ",", ")
+
+    # Year
+    try:
+        formatted_entry += "(" + str(entry['year']) + "). "
+    except KeyError:
+        try:
+            formatted_entry += "(" + str(entry['Year']) + "). "
+        except KeyError:
+            print("No year found in " + str(entry["ID"]))
+
+    # Title
+    formatted_entry += " " + entry["title"].replace("{","").replace("}","")
+
+    # Bibtex box
+    formatted_entry += latex_entry_to_html_box(entry)
+
+    # Closing item
+    formatted_entry += "</li>\n"
+
+
+    return formatted_entry
+
+def create_misc_entry(entry):
+
+    # Starting item
+    formatted_entry = "\n<li>"
+
+    # Authors
+    formatted_entry += entry["author"].replace(" and ",", ")
+
+    # Year
+    try:
+        formatted_entry += "(" + str(entry['year']) + "). "
+    except KeyError:
+        try:
+            formatted_entry += "(" + str(entry['Year']) + "). "
+        except KeyError:
+            print("No year found in " + str(entry["ID"]))
+
+    # Title
+    formatted_entry += " " + entry["title"].replace("{","").replace("}","")
+ 	
+    # Url
+    try:
+        formatted_entry += " (" + str(entry['url']) + "). "
+    except KeyError:
+        try:
+            formatted_entry += " (" + str(entry['Url']) + "). "
+        except KeyError:
+            print("No url found in " + str(entry["ID"]))
+
+    # Bibtex box
+    formatted_entry += latex_entry_to_html_box(entry)
+
+    # Closing item
+    formatted_entry += "</li>\n"
+
+
+    return formatted_entry
